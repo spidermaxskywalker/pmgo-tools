4f0b21635a42aeffb2adbd43e94695ecf37f7788
maxopala@gmail.com <maxopala@gmail.com>
2019-09-23 18:02:23 -0300

    Partiu

--- src/main/java/br/com/maxgontijo/pmgo/planilhasveiculos/jsf/UploadArquivosBean.java
+++ src/main/java/br/com/maxgontijo/pmgo/planilhasveiculos/jsf/UploadArquivosBean.java
@@ -1,13 +1,40 @@
 package br.com.maxgontijo.pmgo.planilhasveiculos.jsf;
 
 import javax.annotation.PostConstruct;
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
+import javax.faces.context.FacesContext;
+
+import org.primefaces.event.FileUploadEvent;
+import org.primefaces.model.UploadedFile;
 
 @ManagedBean
 @ViewScoped
 public class UploadArquivosBean extends GenericJsfBean {
+	private UploadedFile file;
+
 	@PostConstruct
 	public void init() {
+	}
+
+	public UploadedFile getFile() {
+		return file;
+	}
+
+	public void setFile(UploadedFile file) {
+		this.file = file;
+	}
+
+	public void upload() {
+		if (file != null) {
+			FacesMessage message = new FacesMessage("Succesful", file.getFileName() + " is uploaded.");
+			FacesContext.getCurrentInstance().addMessage(null, message);
+		}
+	}
+
+	public void handleFileUpload(FileUploadEvent event) {
+		FacesMessage msg = new FacesMessage("Succesful", event.getFile().getFileName() + " is uploaded.");
+		FacesContext.getCurrentInstance().addMessage(null, msg);
 	}
 }
