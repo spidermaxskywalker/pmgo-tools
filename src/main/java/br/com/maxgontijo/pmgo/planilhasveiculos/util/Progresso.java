f6750c8e6ef89d03732b1f6e442528b845143bc1
Max Gontijo de Oliveira <maxopala@gmail.com>
2020-07-31 00:23:39 -0300

    Ferramenta de conversão de coordenadas para endereço

---
+++ src/main/java/br/com/maxgontijo/pmgo/planilhasveiculos/util/Progresso.java
@@ -0,0 +1,32 @@
+package br.com.maxgontijo.pmgo.planilhasveiculos.util;
+
+public class DownlodProgress {
+    private int total;
+    private int completo;
+
+    public void incrementarProgresso() {
+        if (completo < total) {
+            completo++;
+        }
+    }
+
+    public int getTotal() {
+        return total;
+    }
+
+    public void setTotal(int total) {
+        this.total = total;
+    }
+
+    public int getCompleto() {
+        return completo;
+    }
+
+    public void setCompleto(int completo) {
+        this.completo = completo;
+    }
+
+    public double getProgresso() {
+        return total > 0 ? completo / (double) total : 1;
+    }
+}
